d9588cd64c6f586abdbd245712d4e02165fad612
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 14:34:20 +0500

    creates serializer, viewset and router for certificates

--- certificates/views.py
+++ certificates/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from rest_framework import serializers, viewsets
+from .models import Certificate
 # Create your views here.
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ('id', 'name', 'description', 'created_at', 'updated_at')
+
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
