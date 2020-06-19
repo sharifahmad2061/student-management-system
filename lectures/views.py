fd06ed7c011fbe555d4e92132737681c3c70da93
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 15:43:53 +0500

    creates the viewset for lecture

--- lectures/views.py
+++ lectures/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import LectureSerializer
+from .models import Lecture
 # Create your views here.
+
+
+class LectureViewSet(viewsets.ModelViewSet):
+    queryset = Lecture.objects.all()
+    serializer_class = LectureSerializer
