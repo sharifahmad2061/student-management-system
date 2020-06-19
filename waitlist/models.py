a5d83f5f083e2b689e44a74a99ce0a6a8266e76a
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 14:56:37 +0500

    creates the waitlist model

--- waitlist/models.py
+++ waitlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from utils.models import Timestamps
 # Create your models here.
+
+
+class Waitlist(Timestamps, models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    notes = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Waitlist'
