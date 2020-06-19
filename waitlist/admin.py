c1110f076bb2efc5a6af69fbb8f7d7e9eea2645e
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 14:56:27 +0500

    creates the admin view for waitlist model

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Waitlist
 # Register your models here.
+
+
+@admin.register(Waitlist)
+class WaitlistAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'created_at')
+    search_fields = ('first_name', 'last_name')
