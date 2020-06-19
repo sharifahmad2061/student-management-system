3fba1b00c514910d20539773fc209aaf78af9a1d
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 15:04:02 +0500

    creates lecture model

--- lectures/models.py
+++ lectures/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from utils.models import Timestamps
 
 # Create your models here.
+
+
+class Lecture(Timestamps, models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    slides_url = models.URLField()
+    lecturer_name = models.CharField(max_length=100, null=True, blank=True)
