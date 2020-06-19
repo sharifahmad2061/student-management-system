8e10771e80e95b0c28d8c5c8cd4f749439dce710
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 15:23:40 +0500

    changes the lecture model, adds duration and is_required and makes migrations

--- lectures/models.py
+++ lectures/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from utils.models import Timestamps
 
 # Create your models here.
@@ -7,6 +8,10 @@ from utils.models import Timestamps
 class Lecture(Timestamps, models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    date = models.DateTimeField()
-    slides_url = models.URLField()
     lecturer_name = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField()
+    duration = models.PositiveSmallIntegerField(
+        help_text='Enter number of hours', validators=[
+            MinValueValidator(1), MaxValueValidator(10)])
+    slides_url = models.URLField()
+    is_required = models.BooleanField(default=False)
