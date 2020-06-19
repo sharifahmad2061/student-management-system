b213855f41b06ebc1e0248d45aeba186545f3f56
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 10:47:10 +0500

    changed the admin for CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, StudentProfile
 # Register your models here.
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    model = CustomUser
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ['email', 'is_student', 'is_staff', 'is_admin']
+    list_filter = ['is_student', 'is_admin']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_student', 'is_staff', 'is_admin')})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        })
+    )
+    search_fields = ['email', ]
+    ordering = ['email', ]
+    filter_horizontal = ()
+
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    pass
