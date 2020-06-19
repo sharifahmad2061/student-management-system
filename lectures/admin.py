9ac32d346ab470606e4e3c4a5f043dcbbc60ac1c
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 15:24:58 +0500

    register the admin class for Lecture

--- lectures/admin.py
+++ lectures/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Lecture
 # Register your models here.
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lecturer_name', 'date')
+    search_fields = ('title', 'lecturer_name')
