cd036b3c31824d27d79234ba1b8ad9693a96aa6e
sharifahmad <sharifahmad2061@gmail.com>
2020-06-19 09:30:59 +0500

    creates djangorestframework serializer, viewset and router

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'is_student')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
